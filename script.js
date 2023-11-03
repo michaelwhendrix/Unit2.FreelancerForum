f09c10976a548b318115d42c56e5fe283225e722
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-02 21:32:33 -0500

    completely reworked data into table

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const freelancers = [
     { name: "Dr. Wire", price: 47, occupation: "teacher" },
     { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
-  const liArray =[];
+  const trTableArray =[];
+  const thNameArray =[];
+  const thOccupationArray =[];
+  const thPriceArray =[];
 
   const calcAverage =() => {
     let sumAllFreelancers = 0;
@@ -25,35 +28,44 @@ const infoBox = document.createElement('div');
 const h1Title = document.createElement('h1');
 const h2Average = document.createElement('h2');
 const h1Subtotle = document.createElement('h1');
-const listBox = document.createElement('div');
-const listBoxHeaders = document.createElement('h2');
-const olFreelancers = document.createElement('ol');
+const tableBox = document.createElement('table');
+const tableBoxRow = document.createElement('tr');
+const thName = document.createElement('th');
+const thOccupation = document.createElement('th');
+const thPrice = document.createElement('th');
+
 for(let i =0; i < freelancers.length; i++) {
-    liArray[i] = document.createElement('li');
+    trTableArray[i] =document.createElement('tr');
+    thNameArray[i] = document.createElement('th');
+    thOccupationArray[i] = document.createElement('th');
+    thPriceArray[i] = document.createElement('th');
 }
 
 h1Title.innerHTML = "Freelancer Forum";
 h2Average.innerHTML = `The average starting price is $${calcAverage()}.`;
 h1Subtotle.innerHTML = "Available Freelancers";
-listBox.innerHTML = "Available Freelancers test";
 
 
 
 
 document.body.appendChild(infoBox);
-document.body.appendChild(listBox);
+document.body.appendChild(tableBox);
 infoBox.appendChild(h1Title);
 infoBox.appendChild(h2Average);
 infoBox.appendChild(h1Subtotle);
-listBox.appendChild(listBoxHeaders);
-listBox.appendChild(olFreelancers);
 
+document.body.setAttribute("style", "display:flex; flex-direction:column");
 infoBox.setAttribute("style", "border:3px solid black; display:flex; flex-direction:column; align-items:center");
 
 
 for(let i =0; i < freelancers.length; i++) {
     const element = freelancers[i];
-    liArray[i].innerHTML = `${element.name}    ${element.occupation}     ${element.price} `;
-    olFreelancers.appendChild(liArray[i]);
-}
+    thNameArray[i].innerText = element.name;
+    thOccupationArray[i].innerText = element.occupation;
+    thPriceArray[i].innerText = `$${element.price}`;
 
+    tableBox.appendChild(trTableArray[i]);
+    tableBox.appendChild(thNameArray[i]);
+    tableBox.appendChild(thOccupationArray[i]);
+    tableBox.appendChild(thPriceArray[i]);
+}
